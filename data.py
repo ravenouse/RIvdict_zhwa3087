e36de6afe89f961dd33cea01e93f77c9039a71b1
ravenouse <85110830+ravenouse@users.noreply.github.com>
2022-02-28 21:50:21 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,292 @@
+from collections import defaultdict
+from itertools import count
+import json
+import random
+
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset, Sampler
+
+###version change: import new tokenizer to retokenize
+import tokenizers
+from tokenizers import Tokenizer
+# from tokenizers.models import WordPiece
+# from tokenizers.models import BPE
+# from tokenizers.models import Unigram
+#
+# from tokenizers.trainers import WordPieceTrainer
+# from tokenizers.trainers import BpeTrainer
+# from tokenizers.trainers import UnigramTrainer
+
+
+
+BOS = "<seq>"
+EOS = "</seq>"
+PAD = "<pad/>"
+UNK = "<unk/>"
+EN='<en>'
+ES='<es>'
+FR='<fr>'
+IT='<it>'
+RU='<ru>'
+
+language_list1 = ["<en>","<es>","<fr>","<it>","<ru>"]
+language_list2 = ["<en>","<fr>","<ru>"]
+
+refer_dict={"en":4,"es":5,"fr":6,"it":7,"ru":8}
+
+SUPPORTED_ARCHS = ("sgns", "char")
+
+# A dataset is a container object for the actual data
+class JSONDataset(Dataset):
+    """Reads a CODWOE JSON dataset"""
+
+    def __init__(self, file, tokenizer, vocab=None,freeze_vocab=False, maxlen=256):
+        """
+        Construct a torch.utils.data.Dataset compatible with torch data API and
+        codwoe data.
+        args: `file` the path to the dataset file
+              `vocab` a dictionary mapping strings to indices
+              `freeze_vocab` whether to update vocabulary, or just replace unknown items with OOV token
+              `maxlen` the maximum number of tokens per gloss
+        """
+        ### Version Change: add new vocab of retokenization
+        self.tokenizer_dir = tokenizer
+        self.new_vocab =None
+        self.new_tokenizer()
+
+
+        if vocab is None:
+            self.vocab = defaultdict(count().__next__)
+        else:
+            self.vocab = defaultdict(count(len(vocab)).__next__)
+            self.vocab.update(vocab)
+        pad, eos, bos, unk = (
+            self.vocab[PAD],
+            self.vocab[EOS],
+            self.vocab[BOS],
+            self.vocab[UNK],
+        )
+        if freeze_vocab:
+            self.vocab = dict(vocab)
+        with open(file, "r") as istr:
+            self.items = json.load(istr)
+
+        type = str(file).split("/")[-1]
+        print(type)
+        # preparse data
+        for idx, json_dict in enumerate(self.items):
+            # in definition modeling test datasets, gloss targets are absent
+            json_dict['idx'] = idx
+            if "all" in type and 'electra' not in type:
+                if "train" in type:
+                    json_dict['language_token'] = language_list1[idx//43608]
+                    # print('train file')
+                elif "dev" in type:
+                    json_dict['language_token'] = language_list1[idx //6375]
+                    # print('dev file')
+            elif "all" in type and 'electra' in type:
+                if "train" in type:
+                    json_dict['language_token'] = language_list2[idx//43608]
+                    # print('train file')
+                elif "dev" in type:
+                    json_dict['language_token'] = language_list2[idx //6375]
+                    # print('dev file')
+            else:
+                json_dict['language_token'] = f"<{type[:2]}>"
+
+
+            if "gloss" in json_dict:
+                json_dict["gloss_tensor"] = torch.tensor(
+                    # [refer_dict[language]]+
+                    [bos]
+                    + [
+                        self.vocab[word]
+                        if not freeze_vocab
+                        else self.vocab.get(word, unk)
+                        for word in json_dict["gloss"].split()
+                    ]
+                    + [eos]
+                )
+                if maxlen:
+                    json_dict["gloss_tensor"] = json_dict["gloss_tensor"][:maxlen]
+            # in reverse dictionary test datasets, vector targets are absent
+            for arch in SUPPORTED_ARCHS:
+                if arch in json_dict:
+                    json_dict[f"{arch}_tensor"] = torch.tensor(json_dict[arch])
+            if "electra" in json_dict:
+                json_dict["electra_tensor"] = torch.tensor(json_dict["electra"])
+        self.has_gloss = "gloss" in self.items[0]
+        self.has_vecs = SUPPORTED_ARCHS[0] in self.items[0]
+        self.has_electra = "electra" in self.items[0]
+        self.itos = sorted(self.vocab, key=lambda w: self.vocab[w])
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, index):
+        return self.items[index]
+
+    # we're adding this method to simplify the code in our predictions of
+    # glosses
+    def decode(self, tensor):
+        """Convert a sequence of indices (possibly batched) to tokens"""
+        with torch.no_grad():
+            if tensor.dim() == 2:
+                # we have batched tensors of shape [Seq x Batch]
+                decoded = []
+                for tensor_ in tensor.t():
+                    decoded.append(self.decode(tensor_))
+                return decoded
+            else:
+                return " ".join(
+                    [self.itos[i.item()] for i in tensor if i != self.vocab[PAD]]
+                )
+
+    def save(self, file):
+        torch.save(self, file)
+
+
+
+    @staticmethod
+    def load(file):
+        return torch.load(file)
+
+    ## initialize a new tokenizer
+    def new_tokenizer(self):
+        tokenizer = Tokenizer.from_file(str(self.tokenizer_dir))
+        self.new_vocab = tokenizer.get_vocab()
+        # print(1)
+        return self.new_vocab
+
+# A sampler allows you to define how to select items from your Dataset. Torch
+# provides a number of default Sampler classes
+class TokenSampler(Sampler):
+    """Produce batches with up to `batch_size` tokens in each batch"""
+
+    def __init__(
+        self, dataset, batch_size=200, size_fn=len, drop_last=False, shuffle=True
+    ):
+        """
+        args: `dataset` a torch.utils.data.Dataset (iterable style)
+              `batch_size` the maximum number of tokens in a batch
+              `size_fn` a callable that yields the number of tokens in a dataset item
+              `drop_last` if True and the data can't be divided in exactly the right number of batch, drop the last batch
+              `shuffle` if True, shuffle between every iteration
+        """
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.size_fn = size_fn
+        self._len = None
+        self.drop_last = drop_last
+        self.shuffle = True
+
+    def __iter__(self):
+        indices = range(len(self.dataset))
+        if self.shuffle:
+            indices = list(indices)
+            random.shuffle(indices)
+        i = 0
+        selected = []
+        numel = 0
+        longest_len = 0
+        for i in indices:
+            if numel + self.size_fn(self.dataset[i]) > self.batch_size:
+                if selected:
+                    yield selected
+                selected = []
+                numel = 0
+            numel += self.size_fn(self.dataset[i])
+            selected.append(i)
+        if selected and not self.drop_last:
+            yield selected
+
+    def __len__(self):
+        if self._len is None:
+            self._len = (
+                sum(self.size_fn(self.dataset[i]) for i in range(len(self.dataset)))
+                // self.batch_size
+            )
+        return self._len
+
+
+# DataLoaders give access to an iterator over the dataset, using a sampling
+# strategy as defined through a Sampler.
+def get_dataloader(dataset, batch_size=200, shuffle=True):
+    """produce dataloader.
+    args: `dataset` a torch.utils.data.Dataset (iterable style)
+          `batch_size` the maximum number of tokens in a batch
+          `shuffle` if True, shuffle between every iteration
+    """
+    # some constants for the closures
+    has_gloss = dataset.has_gloss
+    has_vecs = dataset.has_vecs
+    has_electra = dataset.has_electra
+    PAD_idx = dataset.vocab[PAD]
+
+    # the collate function has to convert a list of dataset items into a batch
+    def do_collate(json_dicts):
+        """collates example into a dict batch; produces ands pads tensors"""
+        batch = defaultdict(list)
+        for jdict in json_dicts:
+            for key in jdict:
+                batch[key].append(jdict[key])
+        if has_gloss:
+            batch["gloss_tensor"] = pad_sequence(
+                batch["gloss_tensor"], padding_value=PAD_idx, batch_first=False
+            )
+            # tokenizer = Tokenizer.from_file('tokenizers/tokenizer.json')
+            # new_vocab=tokenizer.get_vocab()
+            # new_gloss_tensor = []
+            # new_gloss_tensor = []
+            # output = tokenizer.encode_batch(batch["gloss"])
+            # for n in range(len(output)):
+            #     one_gloss = [new_vocab[BOS]] + output[n].ids + [new_vocab[EOS]]
+            #     one_gloss_tensor = torch.tensor(one_gloss)
+            #     new_gloss_tensor.append(one_gloss_tensor)
+            # new_gloss_tensor = pad_sequence(new_gloss_tensor, batch_first=True, padding_value=new_vocab[PAD]).T
+            # batch["new_gloss_tensor"] = new_gloss_tensor
+        if has_vecs:
+            for arch in SUPPORTED_ARCHS:
+                batch[f"{arch}_tensor"] = torch.stack(batch[f"{arch}_tensor"])
+        if has_electra:
+            batch["electra_tensor"] = torch.stack(batch["electra_tensor"])
+
+
+        return dict(batch)
+
+
+    if dataset.has_gloss:
+        # we try to keep the amount of gloss tokens roughly constant across all
+        # batches.
+        def do_size_item(item):
+            """retrieve tensor size, so as to batch items per elements"""
+            return item["gloss_tensor"].numel()
+
+        return DataLoader(
+            dataset,
+            collate_fn=do_collate,
+            batch_sampler=TokenSampler(
+                dataset, batch_size=batch_size, size_fn=do_size_item, shuffle=shuffle
+            ),
+        )
+    else:
+        # there's no gloss, hence no gloss tokens, so we use a default batching
+        # strategy.
+        return DataLoader(
+            dataset, collate_fn=do_collate, batch_size=batch_size, shuffle=shuffle
+        )
+
+
+def retokenize(batch,dir):
+    tokenizer = Tokenizer.from_file(str(dir))
+    new_vocab = tokenizer.get_vocab()
+    new_gloss_tensor = []
+    output = tokenizer.encode_batch(batch["gloss"])
+    for n in range(len(output)):
+        one_gloss = [new_vocab[batch['language_token'][n]]]+ [new_vocab[BOS]] + output[n].ids + [new_vocab[EOS]]
+        one_gloss_tensor = torch.tensor(one_gloss)
+        new_gloss_tensor.append(one_gloss_tensor)
+    new_gloss_tensor = pad_sequence(new_gloss_tensor, batch_first=True, padding_value=new_vocab[PAD]).T
+    batch["new_gloss_tensor"] = new_gloss_tensor
+
---
+++ defmod.py
@@ -0,0 +1,229 @@
+import argparse
+import itertools
+import json
+import logging
+import pathlib
+import sys
+
+logger = logging.getLogger(pathlib.Path(__file__).name)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(
+    logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+)
+logger.addHandler(handler)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+import tqdm
+
+import data
+import models
+
+
+def get_parser(
+    parser=argparse.ArgumentParser(description="run a definition modeling baseline"),
+):
+    parser.add_argument(
+        "--do_train", action="store_true", help="whether to train a model from scratch"
+    )
+    parser.add_argument(
+        "--do_pred", action="store_true", help="whether to produce predictions"
+    )
+    parser.add_argument(
+        "--train_file", type=pathlib.Path, help="path to the train file"
+    )
+    parser.add_argument("--dev_file", type=pathlib.Path, help="path to the dev file")
+    parser.add_argument("--test_file", type=pathlib.Path, help="path to the test file")
+    parser.add_argument(
+        "--device",
+        type=torch.device,
+        default=torch.device("cpu"),
+        help="path to the train file",
+    )
+    parser.add_argument(
+        "--source_arch",
+        type=str,
+        default="sgns",
+        choices=("sgns", "char", "electra"),
+        help="embedding architecture to use as source",
+    )
+    parser.add_argument(
+        "--summary_logdir",
+        type=pathlib.Path,
+        default=pathlib.Path("logs") / "defmod-baseline",
+        help="write logs for future analysis",
+    )
+    parser.add_argument(
+        "--save_dir",
+        type=pathlib.Path,
+        default=pathlib.Path("models") / "defmod-baseline",
+        help="where to save model & vocab",
+    )
+    parser.add_argument(
+        "--pred_file",
+        type=pathlib.Path,
+        default=pathlib.Path("defmod-baseline-preds.json"),
+        help="where to save predictions",
+    )
+    return parser
+
+
+def train(args):
+    assert args.train_file is not None, "Missing dataset for training"
+    # 1. get data, vocabulary, summary writer
+    logger.debug("Preloading training data")
+    ## make datasets
+    train_dataset = data.JSONDataset(args.train_file)
+    if args.dev_file:
+        dev_dataset = data.JSONDataset(args.dev_file, vocab=train_dataset.vocab)
+    ## assert they correspond to the task
+    assert train_dataset.has_gloss, "Training dataset contains no gloss."
+    if args.source_arch == "electra":
+        assert train_dataset.has_electra, "Training datatset contains no vector."
+    else:
+        assert train_dataset.has_vecs, "Training datatset contains no vector."
+    if args.dev_file:
+        assert dev_dataset.has_gloss, "Development dataset contains no gloss."
+        if args.source_arch == "electra":
+            assert dev_dataset.has_electra, "Development dataset contains no vector."
+        else:
+            assert dev_dataset.has_vecs, "Development dataset contains no vector."
+    ## make dataloader
+    train_dataloader = data.get_dataloader(train_dataset)
+    dev_dataloader = data.get_dataloader(dev_dataset, shuffle=False)
+    ## make summary writer
+    summary_writer = SummaryWriter(args.summary_logdir)
+    train_step = itertools.count()  # to keep track of the training steps for logging
+
+    # 2. construct model
+    logger.debug("Setting up training environment")
+
+    model = models.DefmodModel(dev_dataset.vocab).to(args.device)
+    model.train()
+
+    # 3. declare optimizer & criterion
+    ## Hyperparams
+    EPOCHS, LEARNING_RATE, BETA1, BETA2, WEIGHT_DECAY = 10, 1.0e-4, 0.9, 0.999, 1.0e-6
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=LEARNING_RATE,
+        betas=(BETA1, BETA2),
+        weight_decay=WEIGHT_DECAY,
+    )
+    criterion = nn.CrossEntropyLoss(ignore_index=model.padding_idx)
+
+    vec_tensor_key = f"{args.source_arch}_tensor"
+
+    # 4. train model
+    for epoch in tqdm.trange(EPOCHS, desc="Epochs"):
+        ## train loop
+        pbar = tqdm.tqdm(
+            desc=f"Train {epoch}", total=len(train_dataset), disable=None, leave=False
+        )
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            vec = batch[vec_tensor_key].to(args.device)
+            gls = batch["gloss_tensor"].to(args.device)
+            pred = model(vec, gls[:-1])
+            loss = criterion(pred.view(-1, pred.size(-1)), gls.view(-1))
+            loss.backward()
+            # keep track of the train loss for this step
+            tokens = gls != model.padding_idx
+            acc = (
+                ((pred.argmax(-1) == gls) & tokens).float().sum() / tokens.sum()
+            ).item()
+            step = next(train_step)
+            summary_writer.add_scalar("defmod-train/xent", loss.item(), step)
+            summary_writer.add_scalar("defmod-train/acc", acc, step)
+            optimizer.step()
+            pbar.update(vec.size(0))
+        pbar.close()
+        ## eval loop
+        if args.dev_file:
+            model.eval()
+            with torch.no_grad():
+                sum_dev_loss = 0.0
+                sum_acc = 0
+                ntoks = 0
+                pbar = tqdm.tqdm(
+                    desc=f"Eval {epoch}",
+                    total=len(dev_dataset),
+                    disable=None,
+                    leave=False,
+                )
+                for batch in dev_dataloader:
+                    vec = batch[vec_tensor_key].to(args.device)
+                    gls = batch["gloss_tensor"].to(args.device)
+                    pred = model(vec, gls[:-1])
+                    sum_dev_loss += F.cross_entropy(
+                        pred.view(-1, pred.size(-1)),
+                        gls.view(-1),
+                        reduction="sum",
+                        ignore_index=model.padding_idx,
+                    ).item()
+                    tokens = gls != model.padding_idx
+                    ntoks += tokens.sum().item()
+                    sum_acc += ((pred.argmax(-1) == gls) & tokens).sum().item()
+                    pbar.update(vec.size(0))
+
+                # keep track of the average loss & acc on dev set for this epoch
+                summary_writer.add_scalar(
+                    "defmod-dev/xent", sum_dev_loss / ntoks, epoch
+                )
+                summary_writer.add_scalar("defmod-dev/acc", sum_acc / ntoks, epoch)
+                pbar.close()
+            model.train()
+
+    # 5. save result
+    model.save(args.save_dir / "model.pt")
+    train_dataset.save(args.save_dir / "train_dataset.pt")
+    dev_dataset.save(args.save_dir / "dev_dataset.pt")
+
+
+def pred(args):
+    assert args.test_file is not None, "Missing dataset for test"
+    # 1. retrieve vocab, dataset, model
+    model = models.DefmodModel.load(args.save_dir / "model.pt")
+    train_vocab = data.JSONDataset.load(args.save_dir / "train_dataset.pt").vocab
+    test_dataset = data.JSONDataset(
+        args.test_file, vocab=train_vocab, freeze_vocab=True, maxlen=model.maxlen
+    )
+    test_dataloader = data.get_dataloader(test_dataset, shuffle=False)
+    model.eval()
+    vec_tensor_key = f"{args.source_arch}_tensor"
+    if args.source_arch == "electra":
+        assert test_dataset.has_electra, "File is not usable for the task"
+    else:
+        assert test_dataset.has_vecs, "File is not usable for the task"
+    # 2. make predictions
+    predictions = []
+    with torch.no_grad():
+        pbar = tqdm.tqdm(desc="Pred.", total=len(test_dataset), disable=None)
+        for batch in test_dataloader:
+            sequence = model.pred(batch[vec_tensor_key].to(args.device))
+            for id, gloss in zip(batch["id"], test_dataset.decode(sequence)):
+                predictions.append({"id": id, "gloss": gloss})
+            pbar.update(batch[vec_tensor_key].size(0))
+        pbar.close()
+    # 3. dump predictions
+    with open(args.pred_file, "a") as ostr:
+        json.dump(predictions, ostr)
+
+
+def main(args):
+    if args.do_train:
+        logger.debug("Performing defmod training")
+        train(args)
+    if args.do_pred:
+        logger.debug("Performing defmod prediction")
+        pred(args)
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+    main(args)
